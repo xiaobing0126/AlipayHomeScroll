b97543e67d479dcc809e552104ba709b9858fa86
宗炳林 <804968295@qq.com>
2018-04-20 09:53:20 +0800

    Initial Commit

---
+++ 仿支付宝首页滑动按钮悬浮到导航栏/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿支付宝首页滑动按钮悬浮到导航栏
+//
+//  Created by 宗炳林 on 2018/4/20.
+//  Copyright © 2018年 宗炳林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 仿支付宝首页滑动按钮悬浮到导航栏/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  仿支付宝首页滑动按钮悬浮到导航栏
+//
+//  Created by 宗炳林 on 2018/4/20.
+//  Copyright © 2018年 宗炳林. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
